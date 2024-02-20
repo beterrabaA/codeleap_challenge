6050b57001926db6e4e85150a2ecc9c234dbe941
Beterraba <ls6182315@gmail.com>
2024-02-20 19:59:26 -0300

    remove unsed atributes magrate

--- api_codeleap/careers/serializers.py
+++ api_codeleap/careers/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username','created_datetime','title','content','author_ip']
+        fields = ['id','username','created_datetime','title','content']
