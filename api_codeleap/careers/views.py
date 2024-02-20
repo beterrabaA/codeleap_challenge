7c9962564d5ebcf6945e880716bfdd2352b9a730
Beterraba <ls6182315@gmail.com>
2024-02-20 19:36:05 -0300

    get userlist

--- api_codeleap/careers/views.py
+++ api_codeleap/careers/views.py
@@ -4,8 +4,11 @@ from careers.serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def user_list(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        
