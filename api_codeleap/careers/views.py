e785db87210bbe700315796cc9fff391bdf67f40
Beterraba <ls6182315@gmail.com>
2024-02-20 20:24:32 -0300

    create user patch method

--- api_codeleap/careers/views.py
+++ api_codeleap/careers/views.py
@@ -9,11 +9,23 @@ from rest_framework.response import Response
 def user_list(request):
     if request.method == 'GET':
         users = User.objects.all()
-        serializer = UserSerializer(users,many=True)
-        return Response(serializer.data)
+        user_serializer = UserSerializer(users,many=True)
+        return Response(user_serializer.data)
     elif request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data,status=status.HTTP_201_CREATED)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['PATCH','DELETE'])
+def user_change_and_delete(request,pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return  Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PATCH':
+        user_serializer = UserSerializer(user,data=request.data,partial=True)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data,status=status.HTTP_200_OK)
