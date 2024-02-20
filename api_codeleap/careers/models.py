25f5402e87671aa050d4c2fb485739019d2535ac
Beterraba <ls6182315@gmail.com>
2024-02-20 19:59:14 -0300

    remove unsed atributes magrate

--- api_codeleap/careers/models.py
+++ api_codeleap/careers/models.py
@@ -5,4 +5,3 @@ class User(models.Model):
     created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=200)
-    author_ip = models.CharField(max_length=100)
