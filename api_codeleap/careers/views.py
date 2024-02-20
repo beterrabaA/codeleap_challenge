a5af56211d12ae69dd552a04df448b6b975fd80d
Beterraba <ls6182315@gmail.com>
2024-02-20 19:33:21 -0300

    create user views

--- api_codeleap/careers/views.py
+++ api_codeleap/careers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from careers.models import User
+from careers.serializers import UserSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def user_list(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
+    return Response(serializer.data)
