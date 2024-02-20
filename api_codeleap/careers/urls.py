3a78f741e60e222a821b1574a4636863b388fe12
Beterraba <ls6182315@gmail.com>
2024-02-20 20:33:13 -0300

    add edit user urls

--- api_codeleap/careers/urls.py
+++ api_codeleap/careers/urls.py
@@ -1,5 +1,5 @@
-from careers.views import user_list
+from careers.views import user_list,user_change_and_delete
 
 from django.urls import path
 
-urlpatterns = [path('',user_list)]
+urlpatterns = [path('',user_list),path('<int:pk>/',user_change_and_delete)]
