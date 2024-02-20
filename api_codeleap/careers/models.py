b59592f35269132dc4fca3733e5da88dd7f729e8
Beterraba <ls6182315@gmail.com>
2024-02-20 18:55:20 -0300

    create user model

--- api_codeleap/careers/models.py
+++ api_codeleap/careers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=200)
+    author_ip = models.CharField(max_length=100)
