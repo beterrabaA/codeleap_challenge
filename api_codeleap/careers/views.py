a20992c5e9857b85b66d37aa0adc5db4fe6eb71a
Beterraba <ls6182315@gmail.com>
2024-02-20 19:39:16 -0300

    userlist post view

--- api_codeleap/careers/views.py
+++ api_codeleap/careers/views.py
@@ -1,6 +1,7 @@
 from careers.models import User
 from careers.serializers import UserSerializer
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -11,4 +12,8 @@ def user_list(request):
         serializer = UserSerializer(users,many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
